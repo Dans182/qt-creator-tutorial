08b447795fb1a010f79936e38cbe75ab49f831fe
Daniel <dgaiteiro@gmail.com>
2023-04-05 00:13:49 +0200

    QTextStream

--- 08-QTextStream3/main.cpp
+++ 08-QTextStream3/main.cpp
@@ -10,5 +10,16 @@ int main(int argc, char *argv[])
     io.setString(&s);
     io << "Hola mundo " << 8 << " " << 9.45;
     qDebug() << s;
+
+    //o se puede hacer de esta manera
+    QString s2 = "12 4.05 Yaaa";
+    QTextStream io2;
+    io2.setString(&s2);
+    int g;
+    double h;
+    QString i;
+    io2 >> g >> h >> i;
+    qDebug() << g << h << i;
+
     return a.exec();
 }
